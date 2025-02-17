1ee72dc8a300f2b2a5e8a06f3f9f216e63e20b6d
lemon <l3mon@lemons-MacBook-Air.local>
2025-02-17 15:18:24 +0530

    handling inputs before sending to db

--- server/db/schema/expenses.ts
+++ server/db/schema/expenses.ts
@@ -1,11 +1,13 @@
 
-import { integer,index, pgTable, varchar, text, numeric , serial} from "drizzle-orm/pg-core";
+import { timestamp } from "drizzle-orm/mysql-core";
+import { integer,index, pgTable, varchar, text, numeric , serial, timestamp} from "drizzle-orm/pg-core";
 
 export const expenses = pgTable("expenses", {
   id: serial("id").primaryKey(),
   userId: text('user_id').notNull(),
   title: text('title').notNull(),
-  amount: numeric('amount', {precision:12 , scale:2}).notNull()
+  amount: numeric('amount', {precision:12 , scale:2}).notNull(),
+  createdAt: timestamp('created_at').defaultNow()
 }, (expenses) => {
     return {
         userIdIndex: index('name_idx').on(expenses.userId)
--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -4,7 +4,7 @@ import { zValidator } from '@hono/zod-validator'
 import {authMiddleware} from '../kinde'
 import { db } from "../db";
 import { expenses as expensesTable } from "../db/schema/expenses";
-import {eq} from 'drizzle-orm'
+import {eq, desc, sum, and} from 'drizzle-orm'
 
 const expenseSchema = z.object({
     id:z.number().int().positive().min(1),
@@ -26,7 +26,12 @@ export const expensesRoute = new Hono()
 .get('/', authMiddleware, async (c) => {
     const user = await c.var.user
     console.log(user.id)
-    const expenses = await db.select().from(expensesTable).where(eq(expensesTable.userId, user.id))
+    const expenses = await db
+    .select()
+    .from(expensesTable)
+    .where(eq(expensesTable.userId, user.id))
+    .orderBy(desc(expensesTable.createdAt))
+    .limit(100)
     return c.json({expenses})
 })
 
@@ -41,28 +46,43 @@ export const expensesRoute = new Hono()
     }).returning()
     return c.json(results)
 })
-.get('/:id{[0-9]+}', authMiddleware, (c) => {
+.get('/:id{[0-9]+}', authMiddleware, async (c) => {
     const id = c.req.param('id')
-    const expense = fakeExpenses.find((exp) => exp.id === Number(id))
+    const user = await c.var.user
+    console.log(user.id)
+    const expense = await db
+    .select()
+    .from(expensesTable)
+    .where(and(eq(expensesTable.userId, user.id), eq(expensesTable.id, id)))
+    .orderBy(desc(expensesTable.createdAt))
+    .limit(1)
+    .then(res => res[0])
+    
     if(!expense){
         return c.notFound()
     }
     return c.json(expense)
 })
-.get('/total-spend', authMiddleware, (c) => {
-    let totalSpend:number = 0
-    fakeExpenses.forEach((expense)=>{
-        totalSpend +=  +expense.amount
-    })
-    return c.json({"total": totalSpend})
+.get('/total-spend', authMiddleware,async (c) => {
+    const user = await c.var.user
+    let result = await db.select({total: sum(expensesTable.amount)}).from(expensesTable).where(eq(expensesTable.userId, user.id)).limit(1).then(res => res[0])
+    
+    return c.json(result)
 })
-.delete('/:id{[0-9]+}', authMiddleware,  (c) => {
+.delete('/:id{[0-9]+}', authMiddleware, async  (c) => {
     const id = c.req.param('id')
-    const index = fakeExpenses.findIndex((exp) => exp.id === Number(id))
-    if(!index){
+    const user = await c.var.user
+    console.log(user.id)
+    const expense = await db
+    .delete(expensesTable)
+    .returning()
+    .where(and(eq(expensesTable.userId, user.id), eq(expensesTable.id, id)))
+
+
+    if(!expense){
         return c.notFound()
     }
-    fakeExpenses.splice(index, 1)
+    
     return c.json({"message": "deleted succesfully"})
 })
 //.put
