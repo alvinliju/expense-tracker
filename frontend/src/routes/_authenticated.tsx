5949de61d154ef11605ac9823d18ea88869cbcd4
lemon <l3mon@lemons-MacBook-Air.local>
2025-02-17 15:42:39 +0530

    added login button

--- frontend/src/routes/_authenticated.tsx
+++ frontend/src/routes/_authenticated.tsx
@@ -1,12 +1,17 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 import { userQueryOptions } from '@/lib/api'
-
+import { Button } from "@/components/ui/button"
+import { useNavigate } from '@tanstack/react-router'
 
 
 const Login = () => {
+   
     return (
-        <div>
+        <div className='p-4 flex flex-col gap-4 '>
             <p>Login required</p>
+            <a href="/api/login">
+            <Button  className='max-w-[50px] hover:cursor-pointer' >Login</Button>
+            </a>
         </div>
     )
 }
